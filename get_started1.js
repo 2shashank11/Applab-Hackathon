6a61bd931d598e7d6a1ad5bb35849e4f0d7d53da
planetaryan <117113732+planetaryan@users.noreply.github.com>
2023-03-19 05:39:15 +0530

    Add files via upload

---
+++ get_started1.js
@@ -0,0 +1,113 @@
+
+
+
+
+	
+	var checkBox=document.getElementById("myCheck1");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Communication is the act of transferring information, ideas, and data from one group to another. Typically, communication involves talking, writing, listening, or reading, combined with more subtle variations, like reading body language and empathy.\n'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck2");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Great leaders execute a collective vision and motivate individuals to put forth their best efforts for the benefit of the company, organization, or team. They manage conflict and make decisions on a daily basis. Excellent managers come with all types of leadership styles.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck3");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Probability is the determination of how likely an event will occur, or how likely something is true, while statistics involves the collection, interpretation, and analysis of data to solve problems. Both are important in careers that involve science, technology, engineering and mathematics.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck4");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Entrepreneurship is the act of creating value, usually by developing an idea into a successful business that provides a valuable service or product. To do this you need to know how to design, organize and manage new enterprises through the stages of launch, growth and financing.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck5");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Computer programming skills are in high demand as work becomes increasingly automated. Programmers write code that instructs how computers, apps, and software programs perform. It involves analyzing, generating, and implementing algorithm.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck6");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Business psychology, combines human psychology with practical business application to improve productivity and organization. Learning business psychology is beneficial in developing strong networks that can help you get jobs and build harmonious teams.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck7");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Business analysis is a way of identifying business needs and solutions. Business analysts play an integral role in making data-driven decisions based on research and strategic thinking. You will need to present your findings to management in roles that require business analysis.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+	var checkBox=document.getElementById("myCheck8");
+    checkBox.addEventListener('change', function() {
+	  if (this.checked  ) {
+		console.log("checked");
+		
+		var para='Data analysis involves gathering, cleaning, analyzing, visualizing, and presenting data to inform business decisions. As a data analyst, you leverage data to solve problems and help stakeholders navigate the best plan of action. Learning data analysis can lead to a high-impact career.'
+		document.getElementById('skill').append(para)
+		document.createElement('br')
+	}
+
+	   
+	})
+
+
+
+   
+
+    
+
+
+					
+
+
+
